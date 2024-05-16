1510d7b10eb796c1e5421d7cd62278f35313db52
Hung Huang <hung_h102@icloud.com>
2024-05-16 14:40:56 +0930

    hm

--- VMTranslator/VMTranslator.cpp
+++ VMTranslator/VMTranslator.cpp
@@ -21,35 +21,24 @@ VMTranslator::~VMTranslator() {
 /** Generate Hack Assembly code for a VM push operation */
 string VMTranslator::vm_push(string segment, int offset){
 
-    string ASM = "";
-    string seg = "";
-
     if (segment == "local"){
-        ASM += "@" + to_string(offset) + "\nD=A\n" + "@LCL" + "\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
-        return ASM;
+        return "@" + to_string(offset) + "\nD=A\n" + "@LCL" + "\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
     } else if (segment == "argument"){
-        ASM += "@" + to_string(offset) + "\nD=A\n" + "@ARG" + "\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
-        return ASM;
+        return "@" + to_string(offset) + "\nD=A\n" + "@ARG" + "\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
     } else if (segment == "this"){
-        ASM += "@" + to_string(offset) + "\nD=A\n" + "@THIS" + "\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
-        return ASM;
+        return "@" + to_string(offset) + "\nD=A\n" + "@THIS" + "\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
     } else if (segment == "that"){
-        ASM += "@" + to_string(offset) + "\nD=A\n" + "@THAT" + "\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
-        return ASM;
+        return "@" + to_string(offset) + "\nD=A\n" + "@THAT" + "\nA=M+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
     } else if (segment == "temp"){
-        ASM += "@" + to_string(offset) + "\nD=A\n@5\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
-        return ASM;
+        return "@" + to_string(offset) + "\nD=A\n@5\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
     } else if (segment == "static"){
-        ASM += "@" + to_string(offset) + "\nD=A\n@16\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
-        return ASM;
+        return "@" + to_string(offset) + "\nD=A\n@16\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
     } else if (segment == "pointer") {
-        ASM += "@" + to_string(offset) + "\nD=A\n@3\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
-        return ASM;
+        return "@" + to_string(offset) + "\nD=A\n@3\nA=A+D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
     } else if (segment == "constant") {
-        ASM += "@" + to_string(offset) + "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
-        return ASM;
+        return "@" + to_string(offset) + "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
     }
-    return ASM;
+    return "";
 }
 
 /** Generate Hack Assembly code for a VM pop operation */
@@ -112,34 +101,30 @@ string VMTranslator::vm_pop(string segment, int offset){
 
 /** Generate Hack Assembly code for a VM add operation */
 string VMTranslator::vm_add(){
-    string ASM = "";
-    ASM += vm_pop("temp", 8) + vm_pop("temp", 9) + "@14\nD=M\n@13\nM=M+D\n" + vm_push("temp", 8);
-    return ASM;
+    return vm_pop("temp", 8) + vm_pop("temp", 9) + "@14\nD=M\n@13\nM=M+D\n" + vm_push("temp", 8);
 }
 
 /** Generate Hack Assembly code for a VM sub operation */
 string VMTranslator::vm_sub(){
-    string ASM = "";
-    ASM += vm_pop("temp", 8) + vm_pop("temp", 9) + "@13\nD=M\n@14\nM=M-D\n" + vm_push("temp", 9);
-    return ASM;
+    return vm_pop("temp", 8) + vm_pop("temp", 9) + "@13\nD=M\n@14\nM=M-D\n" + vm_push("temp", 9);
+
 }
 
 /** Generate Hack Assembly code for a VM neg operation */
 string VMTranslator::vm_neg(){
-    string ASM = "";
-    ASM += vm_pop("temp", 8) += "@13\nM=!M\nM=M+1\n" + vm_push("temp", 8);
-    return ASM;
+    return vm_pop("temp", 8) += "@13\nM=!M\nM=M+1\n" + vm_push("temp", 8);
+
 }
 
 /** Generate Hack Assembly code for a VM eq operation */
 string VMTranslator::vm_eq(){
     string ASM = "";
     ASM += vm_pop("temp", 8) + vm_pop("temp", 9);
-    ASM += "@13\nD=M\n@VMPOS\nD;JGT\n";
-    ASM += "@14\nD=M\n@VMPOS2\nD;JGT\n";
+    ASM += "@13\nD=M\n@VMPOS\nD;JGE\n";
+    ASM += "@14\nD=M\n@VMPOS2\nD;JGE\n";
     ASM += "(VMNEG)\n@14\nD=M\n@13\nD=D-M\n@VMeq\nD;JEQ\n@13\nM=0\n@VMeqend1\n0;JMP\n(VMeq)\n@13\nM=-1\n(VMeqend1)\n@VMeqend2\n0;JMP\n";
     ASM += "(VMPOS2)\n@13\nM=0\n@VMeqend2\n0;JMP\n";
-    ASM += "(VMPOS)\n@14\nD=M\n@VMNEG\nD;JGT\n";
+    ASM += "(VMPOS)\n@14\nD=M\n@VMNEG\nD;JGE\n";
     ASM += "@13\nM=0\n(VMeqend2)\n" + vm_push("temp", 8);
     return ASM;
 }
